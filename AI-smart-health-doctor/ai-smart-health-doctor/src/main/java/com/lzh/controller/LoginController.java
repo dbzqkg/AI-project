c1307aa250b9f043b3f2f965793d84abbf99a6c8
林子涵 <3233500864@qq.com>
2026-03-03 16:22:26 +0800

    Update LoginController.java

--- AI-smart-health-doctor/ai-smart-health-doctor/src/main/java/com/lzh/controller/LoginController.java
+++ AI-smart-health-doctor/ai-smart-health-doctor/src/main/java/com/lzh/controller/LoginController.java
@@ -6,6 +6,8 @@ import com.lzh.pojo.Result;
 import com.lzh.pojo.User;
 import com.lzh.service.LoginService;
 import com.lzh.utils.JwtUtils;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -28,8 +30,15 @@ public class LoginController {
     }
 
 
-    @GetMapping("/profiles/{userId}")
-    public Result getProfiles(@PathVariable Integer userId) {
+    @GetMapping("/profiles")
+    public Result getProfiles(HttpServletRequest request) {
+        // 1. 从请求头获取 token
+        String token = request.getHeader("token");
+        // 2. 解析 token 获取其中的 userId
+        Claims claims = JwtUtils.parseJwt(token);
+        Integer userId = (Integer) claims.get("userId");
+
+        // 3. 根据自己的 userId 查自己的就诊人列表
         List<PatientProfile> list = loginService.getProfiles(userId);
         return Result.success(list);
     }
